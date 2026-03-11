4a956b7fd588ac42417c062571a2285425c29d97
kirakira <15catinthebox@gmail.com>
2026-03-11 16:55:39 -0700

    Add event listeners for email and password inputs; implement form submission alert

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const emailInput = document.getElementById("email");
 const emailLeftIcon = document.getElementById("email-left-icon");
 const passwordInput = document.getElementById("password");
 const passwordLeftIcon = document.getElementById("password-left-icon");
+const loginForm = document.querySelector(".form");
+const remindMeInput = document.querySelector(".form__input-checkbox");
 
 emailInput.addEventListener("focus", () => {
   emailLeftIcon.src = "images/mail-focus.svg";
@@ -17,4 +19,12 @@ passwordInput.addEventListener("focus", () => {
 
 passwordInput.addEventListener("blur", () => {
   passwordLeftIcon.src = "images/lock.svg";
+});
+
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  alert(
+    `Email: ${emailInput.value}\nPassword: ${passwordInput.value}\nRemind me: ${remindMeInput.checked}`
+  );
 });
